241f6d88ed26b7e5de0704b5e08f743a218603f3
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2021-01-10 19:46:55 -0300

    add staff permission to create and view menu

--- menus/views.py
+++ menus/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,16 +10,22 @@ from menus.forms import CreateMenuForm
 from menus.models import Menu
 
 
-class CreateMenuView(LoginRequiredMixin, CreateView):
+class CreateMenuView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     login_url = 'login'
     form_class = CreateMenuForm
     template_name = 'create_menu.html'
+
+    def test_func(self):
+        return self.request.user.is_staff
 
     def get_success_url(self):
         return reverse('detail_menu', kwargs={'pk': self.object.id})
 
 
-class DetailMenuView(LoginRequiredMixin, DetailView):
+class DetailMenuView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = Menu
     context_object_name = 'menu'
     template_name = 'detail.html'
+
+    def test_func(self):
+        return self.request.user.is_staff

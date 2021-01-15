87ca1328a3b2b208211f6df831f6e584e0eb21a9
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2021-01-15 03:00:34 -0300

    add users creation tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from users.models import Employee
+from django.test import Client
+
+
+class UserCreationTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.client = Client()
+
+    def test_should_render_view(self):
+        response = self.client.get(reverse('users:signup_manager'))
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        templates = map(lambda t: t.name, response.templates)
+        self.assertIn('signup.html', templates)
+
+    def test_should_create_manager(self):
+        old_count = Employee.objects.filter(is_staff=True).count()
+        data = {
+            'username': 'test_user',
+            'password1': 'Very_secure123$',
+            'password2': 'Very_secure123$',
+            'first_name': 'Test name',
+            'last_name': 'Test last name'
+        }
+
+        response = self.client.post(reverse('users:signup_manager'), data=data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+        new_count = Employee.objects.filter(is_staff=True).count()
+        self.assertEqual(new_count, old_count + 1)
+
+    def test_should_create_employee(self):
+        old_count = Employee.objects.filter(is_staff=False).count()
+        data = {
+            'username': 'test_user',
+            'password1': 'Very_secure123$',
+            'password2': 'Very_secure123$',
+            'first_name': 'Test name',
+            'last_name': 'Test last name'
+        }
+
+        response = self.client.post(reverse('users:signup_employee'), data=data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+        new_count = Employee.objects.filter(is_staff=False).count()
+        self.assertEqual(new_count, old_count + 1)
+
